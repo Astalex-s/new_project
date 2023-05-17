515f987013e196537aa618824ef96a6c1ddd7eaf
Alexey <astaf.al.mi@gmail.com>
2023-05-18 02:01:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+let a = 10;
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+
+confirm('insert name')
+// типы данных
+/*var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("string"*9);*/
+// let something;
+// console.log(something);
+
+// let persone = {
+//     name:"Jone",
+//     age:25,
+//     isMarried:false
+// };
+
+// console.log(persone["name"]);
+// let arr = ['plam.png','orange.ipg','apple.bmp'];
+// console.log(arr[0])
+
+//alert('Hello World!');
+
+// let answer = confirm('Are you here?');
+// console.log(answer);
+// let answer = prompt('Есть ли вам 18?', "Да");
+// console.log(typeof(null));
+
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(incr++);
+// console.log(decr--);
