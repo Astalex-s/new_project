305adf092a03dfc2587691bce6277d5a90a81671
Alexey <astaf.al.mi@gmail.com>
2023-06-05 01:48:17 +0500

    three lesson

--- script.js
+++ script.js
@@ -47,13 +47,13 @@ let num = 50;
 // }
 // while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        continue;
-    }
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         continue;
+//     }
     
-    console.log(i)
-}
+//     console.log(i)
+// }
 
 
 
@@ -133,3 +133,100 @@ console.log("string"*9);*/
 //         i++
 //     }
 // while (i < 2);
+
+//--------------------------------------------------------------------------------------------------
+// // callback function
+
+// function first(){
+//     //что-то делаем
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+// }
+
+// function second(){
+//     console.log(2);
+// }
+
+// first();
+// second();
+
+// // CALLBACK FUNCTION
+
+// function learnJS(leng, callback) {
+//     console.log("Я учу " + leng);
+//     callback();
+// }
+
+// function done (){
+//     console.log("Я прошел 3-й урок!");
+// }
+
+// learnJS("JavaScript", done)
+
+//OBJECT --------------------------------------------------------------------------------------------------
+
+// let options  = {
+//     width: 1024,
+//     height: 1024,
+//     name: "test"
+// };
+
+// console.log(options.name);
+
+// options.bool = false;  // добавление свойства объекта
+// options.color = {
+//     border: "black",
+//     bg: "red"
+// };
+
+// delete options.bool; // удаление свойства объекта
+// console.log(options);
+
+// for (let key in options) {
+//     console.log('Свойство ' + key + " имеет значение: " + options[key]); // перебор свойств объекта
+// };
+
+// console.log(Object.keys(options).length); // пересчет свойств объекта в числовом формате
+
+//Arrey --------------------------------------------------------------------------------------------------
+
+// let arr = ['first', 2, 3, 'four', 5];
+
+// arr.pop(); // удаление последнего значения в массиве
+// arr.push('5'); // добавление значения в конец массива
+// arr.shift(); // удаление значения в начале массива
+// arr.unshift('1'); // добавление значения в начало массива
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {    //перебор значений массива
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(item, i, mass){
+//     console.log(i + ': ' + item + ' (массив: ' + mass + ')');
+// });
+
+// let mass = [1, 3, 4, 6, 7] // перебор значений массива
+// for (let key of mass){
+//     console.log(key);
+// }
+
+// let ans = prompt('', ''), // преобразует переданные строки в массив через запятую
+//     arr = [];
+// arr = ans.split(',');
+// console.log(arr);
+
+// let arr = ["sjsj", 'sjso', 'afqt', 'ih'], // склеивает значения массива в одну строку
+//     i = arr.join(', ');
+// console.log(i);
+
+let arr = [1, 15, 4], // склеивает значения массива в одну строку
+    i = arr.sort(compareNum);
+
+function compareNum(a,b) { //сортировка массива цифр
+    return a-b;
+}
+    
+console.log(arr);
